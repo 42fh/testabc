3183ad7f05295e361c466b22c04add609cdb5197
fheld <08.analogues-reels@icloud.com>
2024-02-26 13:58:54 +0100

    a

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * @brief This is a hello world example.
+ *
+ * This function prints "Hello, World!" to the console.
+ *
+ * @return 0 on success, -1 on failure.
+ */
+
+int main() {
+    printf("Hello, fish!\n");
+
+    return 0;
+}
